065ce694f74c5945653f1f9ce70010121e9b7a88
Usama <2021se41@student.uet.edu.pk>
2023-10-13 08:48:21 +0500

    Grtting input from user

--- Practice.py
+++ Practice.py
@@ -7,5 +7,18 @@ print(cgpa)
 #input taken from the user 
 a=int(input("age bta:"))
 print(a)
-
+#chcking the type of varialbe 
 print("The type of name is",type(name))
+#Mahermatical operations 
+a=5
+b=4
+print(a+b)
+print(a-b)
+print(a**b);
+
+#task to get the value in integer and float 
+integer=int(input("Enter the number in integer:"))
+fl=float(input("Enter the value in floating points numbers:"))
+print("The Addition :",integer+fl)
+print("The substraction is  :",integer-fl)
+print("The multiplication is  :",integer*fl)
